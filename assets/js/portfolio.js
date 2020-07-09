822d66a219095dd4160373b5f2410e4e1cd321fe
ali-kilinc <alikilinc1986@gmail.com>
2020-07-09 14:56:45 +0300

    update interests section

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -2,37 +2,43 @@ $(document).ready(function () {
 
     let zoneUsa = {
         title: "United States of America",
-        explanation: "I visited USA at 2014. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis laboriosam autem ab delectus sequi perferendis rerum illo. Voluptatibus quaerat eveniet fugiat commodi delectus! Voluptatibus.",
+        explanation: "I visited USA at 2015 for ICDM conference in Atlantic City. I also had opportunity to visit New York. Beatiful cities...",
         carouselItems: [
-            '<img src="assets/imgs/newyork.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">New York</span></h3></div>',
-            '<img src="assets/imgs/atlanticcity.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Atlantic City</span></h3></div>'
+            '<img src="assets/imgs/usa1.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">New York</span></h3></div>',
+            '<img src="assets/imgs/usa2.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Atlantic City</span></h3></div>'
         ]
     };
 
     let zoneEurope = {
         title: "Europe",
-        explanation: "I visited Europe...lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis laboriosam autem ab delectus sequi perferendis rerum illo. Voluptatibus quaerat eveniet fugiat commodi delectus! Voluptatibus.",
+        explanation: "Venice, Florence and Rome were cities I visited in Italy for my honeymoon. Dubrovnik fascinated me, it felt like I'm living in Game of Thrones :). I discovered London by walking 120 km in a week.",
         carouselItems: [
-            '<img src="assets/imgs/newyork.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">New York</span></h3></div>',
-            '<img src="assets/imgs/atlanticcity.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Atlantic City</span></h3></div>'
+            '<img src="assets/imgs/italy.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Italy</span></h3></div>',
+            '<img src="assets/imgs/croatia.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Croatia</span></h3></div>',
+            '<img src="assets/imgs/england.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">United Kingdom</span></h3></div>'   
         ]
     };
 
     let zoneTurkey = {
         title: "Turkey",
-        explanation: "Ohh my sweet hometown...lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis laboriosam autem ab delectus sequi perferendis rerum illo. Voluptatibus quaerat eveniet fugiat commodi delectus! Voluptatibus.",
+        explanation: "Home sweet home... Turkey has breathtaking places that everybody should visit someday.",
         carouselItems: [
-            '<img src="assets/imgs/newyork.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">New York</span></h3></div>',
-            '<img src="assets/imgs/atlanticcity.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Atlantic City</span></h3></div>'
+            '<img src="assets/imgs/turkey4.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Antalya</span></h3></div>',
+            '<img src="assets/imgs/turkey3.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">İzmir</span></h3></div>',
+            '<img src="assets/imgs/turkey2.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">İstanbul</span></h3></div>',
+            '<img src="assets/imgs/turkey1.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Amasya</span></h3></div>'
         ]
     };
 
     let zoneSoutheastAsia = {
         title: "Southeast Asia",
-        explanation: "My dream continent...lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis laboriosam autem ab delectus sequi perferendis rerum illo. Voluptatibus quaerat eveniet fugiat commodi delectus! Voluptatibus.",
+        explanation: "For me, most beautiful places on Earth. I really enjoyed every single second I spent there",
         carouselItems: [
-            '<img src="assets/imgs/newyork.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">New York</span></h3></div>',
-            '<img src="assets/imgs/atlanticcity.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Atlantic City</span></h3></div>'
+            '<img src="assets/imgs/thailand.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Thailand</span></h3></div>',
+            '<img src="assets/imgs/indonesia.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Indonesia</span></h3></div>',
+            '<img src="assets/imgs/vietnam.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Vietnam</span></h3></div>',
+            '<img src="assets/imgs/singapore.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Singapore</span></h3></div>',
+            '<img src="assets/imgs/malaysia.jpg" alt=""><div class="carousel-caption d-none d-md-block"><h3><span class="badge badge-dark light">Malaysia</span></h3></div>'
         ]
     };
 
